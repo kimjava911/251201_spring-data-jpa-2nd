b2b7157d12aa01cab00e993042c901e437ce3a26
kimjava911 <aibackend4th@gmail.com>
2025-12-01 10:19:52 +0900

    step2

--- src/main/java/kr/java/jpa/model/repository/PostRepository.java
+++ src/main/java/kr/java/jpa/model/repository/PostRepository.java
@@ -6,6 +6,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
@@ -31,4 +32,56 @@ public interface PostRepository extends JpaRepository<Post, Long> {
         WHERE p.id = :id
     """)
     Optional<Post> findByIdWithDetails(@Param("id") Long id);
+
+    // Query Method
+    // 1. 제목과 내용으로 검색 + or 조건
+    // findBy - 검색
+    // Title (속성명) Containing (부분일치)
+    // OR Content (속성명) Containing (부분일치)
+    // OrderBy (정렬) CreatedAt (속성명) Desc (차순 - 내림차순 <- default 오름차순이므로 오름차순이면 명시하지 X)
+    List<Post> findByTitleContainingOrContentContainingOrderByCreatedAtDesc(
+            String title, String content);
+
+    // 2. 특정 기간 내 작성된 게시글 조회
+    List<Post> findByCreatedAtBetweenOrderByCreatedAtDesc(
+            LocalDateTime start, LocalDateTime end); // start부터(포함) end까지(포함)
+    // findBy / CreatedAt / Between / OrderBy / CreatedAt / Desc
+
+    // 3. 좋아요 수가 특정 값 이상인 게시글 조회
+    List<Post> findByLikeCountGreaterThanEqualOrderByCreatedAtDesc(Integer minCount);
+    // findBy
+    // LikeCount
+    // GreaterThanEqual : GTE -> GreaterThan (초과, >), LessThan (미만, <)
+    //                           GreaterThanEqual (이상, >=), LessThanEqual (이하, <=)
+    // OrderByCreatedAtDesc
+
+    // 4. 여러 작성자의 게시글 조회
+    @Query("""
+        SELECT p FROM Post p
+                WHERE p.author.id
+                        IN :authorIds
+                ORDER BY p.createdAt DESC
+        """)
+    List<Post> findByAuthorInOrderByCreatedAtDesc(@Param("authorIds") List<Long> authorIds);
+
+    // 5. 제목으로 게시글 존재 여부 확인
+    boolean existsByTitle(String title);
+    // existsBy -> T/F -> 존재여부
+
+    // 6. 특정 작성자의 게시글 개수
+    long countByAuthorId(Long authorId);
+    // count -> 갯수 (집합연수)
+
+    // 7. 복잡한 쿼리, Fetch -> 성능을 위한 쿼리
+    @Query("""
+        SELECT p FROM Post p
+            JOIN FETCH p.author
+            WHERE (p.title LIKE %:keyword% OR p.content LIKE %:keyword%)
+            AND p.likeCount >= :minLikes
+            ORDER BY p.createdAt DESC
+    """)
+    List<Post> searchPosts(
+            @Param("keyword") String keyword,
+            @Param("minLikes") Integer minLikes
+    );
 }
