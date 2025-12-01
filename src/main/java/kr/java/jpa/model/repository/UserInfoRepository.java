bede470b61407c7b7c18b7c54e5ac762e913e6b3
kimjava911 <aibackend4th@gmail.com>
2025-12-01 10:28:50 +0900

    step2

--- src/main/java/kr/java/jpa/model/repository/UserInfoRepository.java
+++ src/main/java/kr/java/jpa/model/repository/UserInfoRepository.java
@@ -6,6 +6,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import java.time.LocalDateTime;
+import java.util.List;
 import java.util.Optional;
 
 public interface UserInfoRepository extends JpaRepository<UserInfo, Long> {
@@ -43,4 +45,38 @@ public interface UserInfoRepository extends JpaRepository<UserInfo, Long> {
            """
    )
    UserInfo findAuthorWithLikedPosts(@Param("userId") Long userId);
+
+   // Query Method
+
+    // 1. 이메일로 사용자 조회
+   Optional<UserInfo> findByEmail(String email);
+
+   // 2. 닉네임으로 검색 (부분 일치)
+   List<UserInfo> findByNicknameContaining(String keyword);
+
+   // 3. 특정 가입한 사용자 조회
+    @Query(
+        """
+        SELECT ui FROM UserInfo ui
+            WHERE ui.createdAt BETWEEN :start AND :end
+            ORDER BY ui.createdAt DESC
+        """
+    )
+   List<UserInfo> findByUserRegisteredBetween(
+       @Param("start") LocalDateTime start,
+       @Param("end") LocalDateTime end
+   );
+
+   // 4. 게시글 수가 특정 개수 이상인 활성 사용자
+   @Query(
+           """
+           SELECT ui FROM UserInfo ui
+               WHERE SIZE(ui.posts) >= :minPosts
+               ORDER BY SIZE(ui.posts) DESC
+           """
+   )
+   // SIZE -> jpql 내장 함수
+   List<UserInfo> findActiveUsers(
+           @Param("minPosts") int minPosts
+   );
 }
